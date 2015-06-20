72cfb0ffcad273633b0a5e512c41b64426854073
Daniel Vera <dvera@bio.fsu.edu>
2015-06-20 14:02:21 -0400

    Update ensemblGffToBed12.sh

--- ensemblGffToBed12.sh
+++ ensemblGffToBed12.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-wget -O- ftp.ensemblgenomes.org/pub/release-22/plants/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.22.gff3.gz | gunzip -c | gffread -T -o- Arabidopsis_thaliana.TAIR10.22.gff3 | sed 's/transcript://g' | sed 's/gene://g' | awk -F '\t' '{ if($2 != "."){print $0} }' OFS='\t' | gtfToGenePred stdin stdout | genePredToBed stdin stdout > out.bed
+outname=$(echo $1 | sed 's/gff3\.gz/bed/g')
+
+wget -O- $1 |\ 
+gunzip -c | gffread -T -o- Arabidopsis_thaliana.TAIR10.22.gff3 |\ 
+sed 's/transcript://g' |\ 
+sed 's/gene://g' |\ 
+awk -F '\t' '{ if($2 != "."){print $0} }' OFS='\t' |\ 
+gtfToGenePred stdin stdout |\ 
+genePredToBed stdin stdout > $outname
+
