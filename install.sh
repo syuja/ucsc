114be08a6ea91cf88a0042ab1e3a8e984fd624af
Daniel Vera <vera@genomics.fsu.edu>
2016-02-10 16:54:17 -0500

    update for centos7

--- install.sh
+++ install.sh
@@ -26,8 +26,13 @@ cp $SCRIPTDIR/my.cnf /etc/my.cnf
 yum install -y man
 iptables -F
 
+#centos6
 yum install -y  mysql mysql-server httpd git mysql-devel.x86_64
-yum install -y gcc libpng-devel-1.5.13-5.el7.x86_64 libstdc++-*
+
+#centos7
+yum install -y mariadb-server.x86_64 httpd git mariadb-devel.x86_64
+
+yum install -y gcc libpng-devel-1.5.13-5.el7.x86_64 libstdc++-* make
 #libimobiledevice-devel.x86_64 libplist-devel.x86_64 usbmuxd-devel.x86_64 
 
 ######################
@@ -37,24 +42,28 @@ yum install -y gcc libpng-devel-1.5.13-5.el7.x86_64 libstdc++-*
 apt-get update -y
 apt-get install -y wget apache2 mysql-server git libpng++-dev gcc libc++-dev libstdc++-4.9-dev make libssl-dev libmysqlclient-dev
 
+
+#########################
 ln -s $WEBROOT /usr/local/apache
+rmdir $WEBROOT/html
 ln -s $WEBROOT $WEBROOT/html
 ln -s $WEBROOT $WEBROOT/htdocs
 ln -s $CGI_BIN /usr/lib/cgi-bin
-ln -s $CGI_BIN $WEBROOT/cgi-bin-$USER
+ln -s $CGI_BIN $WEBROOT/cgi-bin- #if $USER not defined
 ln -s $CGI_BIN $WEBROOT/cgi-bin-root
 
 rsync -avzP rsync://hgdownload.cse.ucsc.edu/cgi-bin/ $CGI_BIN
 rsync -avzP rsync://hgdownload.cse.ucsc.edu/htdocs/ $WEBROOT/
 
-rm $WEBROOT/trash
+rm -f $WEBROOT/trash
 mkdir $WEBROOT/trash
 chmod 777 $WEBROOT/trash
-chown -R www-data:www-data $WEBROOT
+#chown -R www-data:www-data $WEBROOT
+chown -R apache:apache $WEBROOT
 chown -R 755 $WEBROOT
 
 mkdir -p $SWDIR
-ln -s $SWDIR ~/software
+#ln -s $SWDIR ~/software
 mkdir -p $SWDIR/bin/$MACHTYPE
 ln -s $SWDIR/bin ~/bin
 git clone http://genome-source.cse.ucsc.edu/samtabix.git $SWDIR/samtabix
@@ -66,6 +75,7 @@ cd $SWDIR/kent
 git checkout -t -b beta origin/beta
 git pull
 sed -i 's/hgBeacon//g' $SWDIR/kent/src/hg/makefile
+sed -i 's/hgMirror//g' $SWDIR/kent/src/hg/makefile #hgMirror makefile breaks cgi make - -${USER} issue
 cd src
 make clean
 make cgi
@@ -88,8 +98,18 @@ echo 0 > /selinux/enforce
 
 yum update -y 
 yum install -y wget
-wget -O- http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm | rpm -ivh -
+
+
+
+
+cd ~
+curl -o epel.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel.rpm
 yum update -y
+
+
+
+
 yum -y install httpd mysql mysql-server httpd git mysql-devel.x86_64 libimobiledevice-devel.x86_64 libplist-devel.x86_64 usbmuxd-devel.x86_64 gcc libpng*x86* libstdc++-* tcsh R vim nano
 yum -y update
 
@@ -117,6 +137,10 @@ Options +Includes
 </Directory>
 EOF
 service httpd restart
+# for docker, use:
+/usr/sbin/apachectl
+
+
 
 ######################
 ### SITE #############
@@ -162,10 +186,10 @@ cp -f $SWDIR/kent/src/webBlat/webBlat.cfg $CGI_BIN
 ### HG.CONF ##########
 ######################
 
-cp $swdir/kent/src/product/ex.hg.conf $CGI_BIN/hg.conf
+cp $SWDIR/kent/src/product/ex.hg.conf $CGI_BIN/hg.conf
 sed -i 's/defaultGenome=.*/defaultGenome='"$DEFAULTGENOME"'/g' $CGI_BIN/hg.conf
 sed -i 's/wiki\.host=.*/wiki\.host='"$HOST"'/g' $CGI_BIN/hg.conf
-sed -i 's/login\.browserName=.*/login\.browserName='"$BROWSERNAME"'/g'$CGI_BIN/hg.conf
+sed -i 's/login\.browserName=.*/login\.browserName='"$BROWSERNAME"'/g' $CGI_BIN/hg.conf
 sed -i 's/login\.browserAddr=.*/login\.browserAddr=http:\/\/'"$HOST"'/g' $CGI_BIN/hg.conf
 sed -i 's/login\.mailSignature=Greenome Browser Staff=.*/login\.mailSignature=Greenome Browser Staff/g' $CGI_BIN/hg.conf
 sed -i 's/login\.mailReturnAddr=.*/login\.mailReturnAddr='"$EMAILADDRESS"'/g' $CGI_BIN/hg.conf
@@ -173,7 +197,7 @@ sed -i 's/custromTracks\.host=.*/custromTracks\.host=localhost/g' $CGI_BIN/hg.co
 sed -i 's/customTracks\.host=.*/customTracks\.host=localhost/g' $CGI_BIN/hg.conf
 sed -i 's/customTracks\.user=.*/customTracks\.user=readwrite/g' $CGI_BIN/hg.conf
 sed -i 's/customTracks\.password=.*/customTracks\.password=update/g' $CGI_BIN/hg.conf
-sed -i 's/customTracks\.tmpdir=.*/customTracks\.tmpdir='"$WEBROOT"/trash/ct'/g' $CGI_BIN/hg.conf
+sed -i 's/customTracks\.tmpdir=.*/customTracks\.tmpdir='"$WEBROOT/trash/ct"'/g' $CGI_BIN/hg.conf
 
 # PUT IN INFO FOR USER AND PASS HERE FOR CUSTOMTRACKS
 
