e0080c16ad3198cc2341d5b0acc7f3458fe3655a
Daniel Vera <dvera@bio.fsu.edu>
2016-03-15 20:33:23 -0400

    various

--- sql_docker.sh
+++ sql_docker.sh
@@ -1,20 +1,6 @@
-mount -t nfs vault:/vault
-docker run -v /vault:/vault -p 3306:3306 -it centos:7 bash # copy config or add to vault for access
-
+mount -t nfs vault:/vault /vault
+docker run -h $HOSTNAME -v /vault:/vault -p 3306:3306 -it centos:7 bash # copy config or add to vault for access
 #source config
-
 sql_yum.sh
-
-#web_setup.sh
-
-#web_kent.sh
-
-#web_hgconf.sh
-
-sql_update_path.sh
-
-#sql_create.sh
-
+sql_conf.sh
 sql_perms.sh
-
-#ln -s $GBDIR /gbdb #only for w22 which points to files in /gbdb
--- sql_perms.sh
+++
@@ -1,9 +0,0 @@
-
-${MYSQL} -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, CREATE TEMPORARY TABLES on hgcentral.* TO browser@'%' IDENTIFIED BY 'genome';" mysql
-${MYSQL} -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, CREATE TEMPORARY TABLES on hgfixed.* TO browser@'%' IDENTIFIED BY 'genome';" mysql
-${MYSQL} -e "GRANT FILE on *.* TO browser@'%' IDENTIFIED BY 'genome';" mysql
-${MYSQL} -e "GRANT SELECT,FILE on *.* TO readonly@'%' IDENTIFIED BY 'access';" mysql
-${MYSQL} -e "GRANT SELECT on mysql.* TO browser@'%' IDENTIFIED BY 'genome';" mysql
-${MYSQL} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER on hgcentral.* TO readwrite@'%' IDENTIFIED BY 'update';" mysql
-${MYSQL} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER,INDEX on customTrash.* TO readwrite@'%' IDENTIFIED by 'update';" mysql
-${MYSQL} -e "FLUSH PRIVILEGES;"
