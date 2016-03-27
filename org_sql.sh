0d55fe7d1ea279aeeeac9127add300d43c886882
Daniel Vera <dvera@bio.fsu.edu>
2016-03-27 08:07:32 -0400

    verbose to orgSql

--- org_sql.sh
+++ org_sql.sh
@@ -22,8 +22,11 @@ fi
 
 assembly=$1
 
+>&2 echo "downloading db sheet"
+
 wget -qO- "https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key="${DBDBID}"&hl=en&exportFormat=tsv" | tr -d "\r" > $BROWSERDIR/db.tsv && echo "" >> $BROWSERDIR/db.tsv
 
+>&2 echo "recreating assembly db"
 
 ocols=$(head -n 1 $BROWSERDIR/db.tsv)
 
@@ -31,13 +34,21 @@ tail -n+2 $BROWSERDIR/db.tsv | while IFS=$'\t' read -r $ocols
 do
   if [[ "$name" == "$assembly" ]]; then
 
+    >&2 echo "  deleting assembly db"
+
     hgsql -e "drop database $name"
 
+    >&2 echo "  recreating assembl db"
+
     hgsql -e "create database $name"
+
+    >&2 echo "  loading chromInfo"
 
     hgsql $name < $SWDIR/kent/src/hg/lib/chromInfo.sql
 
     hgsql -e "load data local infile \"$GBDIR/$name/chromInfo.tab\" into table $name.chromInfo;"
+
+    >&2 echo "  loading grp and gap tables"
 
     hgsql $name < $SWDIR/kent/src/hg/lib/grp.sql
 
@@ -45,11 +56,17 @@ do
 
     hgLoadSqlTab $name cytoBandIdeo $SWDIR/kent/src/hg/lib/cytoBandIdeo.sql $GBDIR/$name/cytoband.bed
 
+    >&2 echo "  downloading track info"
+
     wget -qO- "https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key="$gid"&hl=en&exportFormat=tsv" | tr -d "\r" > $GBDIR/$name/trackDb.tsv && echo "" >> $GBDIR/$name/trackDb.tsv
+
+    >&2 echo "  creating track database file"
 
     awk -F "\t" '{if(NR==1){split($0,xx,"\t"); nf=NF} ; for (i = 1; i <= nf; i++) print xx[i]" "$i; print ""  }' $GBDIR/$name/trackDb.tsv | grep -Pv ' NA$' | grep -v 'bigDataUrl' | grep -Pv '^_' > $GBDIR/$name/trackDb.ra
 
     cols=$(head -n 1 $GBDIR/$name/trackDb.tsv)
+
+    >&2 echo "  making searches"
 
     cat $GBDIR/$name/trackDb.tsv | while IFS=$'\t' read -r $cols
     do
@@ -62,6 +79,8 @@ do
         echo ""
       fi
     done >> $GBDIR/$name/trackDb.ra
+
+    >&2 echo "  making track description pages"
 
     ### HTML ###
     cols=$(head -n 1 $GBDIR/$name/trackDb.tsv)
@@ -76,17 +95,22 @@ do
       fi
     done
 
+    >&2 echo "making track database"
 
     hgTrackDb $GBDIR/$name $name trackDb $SWDIR/kent/src/hg/lib/trackDb.sql $GBDIR/$name
 
+    >&2 echo "making search database"
+
     hgFindSpec $GBDIR/$name $name hgFindSpec $SWDIR/kent/src/hg/lib/hgFindSpec.sql $GBDIR/$name
+
+    >&2 echo "mysql perms"
 
     hgsql -e "GRANT SELECT on $name.* TO readonly@'%' IDENTIFIED BY 'access';"
     hgsql -e "FLUSH PRIVILEGES;"
 
-
+    >&2 echo "loading track data"
+    
     cols=$(head -n 1 $GBDIR/$name/trackDb.tsv)
-
     tail -n+2 $GBDIR/$name/trackDb.tsv | while IFS=$'\t' read -r $cols
     do
       if [ "$_on" -gt 0 ]; then
