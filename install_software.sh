f5dd07140cdf1a09c6ed5a11d9e5ea725a2d84fc
Daniel Vera <vera@genomics.fsu.edu>
2016-08-28 15:24:30 -0400

    Update install_software.sh

--- install_software.sh
+++ install_software.sh
@@ -3,7 +3,8 @@
 #make software directory
 mkdir -p $SWDIR
 mkdir -p $SWDIR/bin/$MACHTYPE
-
+mkdir -p ${CGI_BIN}
+mkdir -p ${CGI_BIN}-
 
 #grab and compile samtabix
 git clone http://genome-source.cse.ucsc.edu/samtabix.git $SWDIR/samtabix
@@ -17,6 +18,8 @@ git checkout -t -b beta origin/beta
 git pull
 sed -i 's/hgBeacon//g' $SWDIR/kent/src/hg/makefile
 sed -i 's/hgMirror//g' $SWDIR/kent/src/hg/makefile #hgMirror makefile breaks cgi make - -${USER} issue
+
+# add editing of makefiles here
 
 cd $SWDIR/kent/src
 make utils
