283ba0080c1e3f9083b8555b18c622cc96aaf5de
Daniel Vera <vera@genomics.fsu.edu>
2016-08-28 22:05:02 -0400

    Update initiate_browser.sh

--- initiate_browser.sh
+++ initiate_browser.sh
@@ -41,47 +41,28 @@ mysqladmin -u $SQL_USER password $SQL_PASSWORD
 $MYSQL -e "create database hgFixed"
 $MYSQL -e "create database hgcentral"
 $MYSQL -e "create database customTrash"
-curl -o $MYSQLDATA/hgcentral.sql http://hgdownload.cse.ucsc.edu/admin/hgcentral.sql
+curl -so $MYSQLDATA/hgcentral.sql http://hgdownload.cse.ucsc.edu/admin/hgcentral.sql
 $MYSQL hgcentral < $MYSQLDATA/hgcentral.sql # replace this with creating the databases manually or modify sql statements
 
 chown -R mysql:mysql $MYSQLDATA #hgcentral db in mysqldata is created by root!
 chmod -R 755 $MYSQLDATA #necessary?
 
-
-
-
-######################
-### SQL ##############
-######################
-
 # allow an admin node to do anything
 ${MYSQL} -e "GRANT ALL PRIVILEGES on *.* TO root@'%' IDENTIFIED BY '"$SQL_PASSWORD"' WITH GRANT OPTION;" mysql
-
 ${MYSQL} -e "GRANT SELECT on hgFixed.* TO readonly@'%' IDENTIFIED BY 'access';" mysql
 ${MYSQL} -e "GRANT SELECT on hgFixed.* TO readonly@'localhost' IDENTIFIED BY 'access';" mysql
-
 # allow webserver to modify hgcentral and customTrash
 ${MYSQL} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER on hgcentral.* TO readwrite@'%."$DOMAIN"' IDENTIFIED BY 'update';" mysql
 ${MYSQL} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER on hgcentral.* TO readwrite@'localhost' IDENTIFIED BY 'update';" mysql
-
 ${MYSQL} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER,INDEX on customTrash.* TO readwrite@'%."$DOMAIN"' IDENTIFIED by 'update';" mysql
 ${MYSQL} -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER,INDEX on customTrash.* TO readwrite@'localhost' IDENTIFIED by 'update';" mysql
-
 ${MYSQL} -e "FLUSH PRIVILEGES;"
-
 pkill mysqld
-
-######################
-### WEB ##############
-######################
-
-# clear from hgcentral: defaultDb, clade, genomeClade, dbDb, dbDbArch, liftOverChain, hubPublic, targetDb
-# make sure host info is correct for custom tracks
 
 mkdir -p $CGI_BIN
 
-rsync --delete -az rsync://hgdownload.cse.ucsc.edu/cgi-bin/ $CGI_BIN
-rsync --delete -az rsync://hgdownload.cse.ucsc.edu/htdocs/ $WEBROOT/
+rsync -az rsync://hgdownload.cse.ucsc.edu/cgi-bin/ $CGI_BIN
+rsync -az rsync://hgdownload.cse.ucsc.edu/htdocs/ $WEBROOT/
 
 ln -s $WEBROOT $WEBROOT/html
 ln -s $WEBROOT $WEBROOT/htdocs
@@ -90,32 +71,22 @@ ln -s $CGI_BIN /usr/lib/cgi-bin
 ln -s $CGI_BIN $WEBROOT/cgi-bin- #if $USER not defined
 ln -s $CGI_BIN $WEBROOT/cgi-bin-root
 
-
-# get all html files in toplevel
-# directories we want:  admin cgi-bin FAQ
-
-
-
-# delete left toolbar from index
-sed -i '/<TABLE WIDTH="100%" BORDER=0 CELLPADDING=0 CELLSPACING=0>/,/<\/TD><\/TR><\/TABLE>/d' $WEBROOT/index.html
-
 rm -fr $WEBROOT/trash
 mkdir -p $WEBROOT/trash
 chmod 777 $WEBROOT/trash
 chown -R apache:apache $WEBROOT
 chown -R 755 $WEBROOT
 
-# RUN ON WEB SERVER
-
 cd $SWDIR/kent/src
 make clean
 make cgi
 make blatSuite
 
-#post-build tasks
+# copy webBlat to cgi-bin
 rm -f $CGI_BIN/webBlat
 cp -f $SWDIR/kent/src/webBlat/webBlat $CGI_BIN
 rm -f $CGI_BIN/webBlat.cfg
 cp -f $SWDIR/kent/src/webBlat/webBlat.cfg $CGI_BIN
 
-chown -R apache:apache $WEBROOT #cgis copied from source compilaton owned by root
+# cgis copied from source compilaton owned by root
+chown -R apache:apache $WEBROOT
